819d1f11f1bfaf1a7f814908c3e839615c4736ca
christanplaza <christan.shane.plaza@sun-asterisk.com>
2020-01-23 22:06:23 +0800

    Reply Form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>SunOverflow*</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -20,12 +20,12 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
-<body>
+<body class="mb-4">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <span style="color: #FF2200">Sun*</span>Overflow
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -34,7 +34,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="/threads">All Threads</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -22,5 +22,27 @@
             @include ('threads.reply')
         @endforeach
     </div>
+
+    @if (auth()->check())
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="card p-4 shadow">
+                    <form action="{{ $thread->path() . '/replies' }}" method="POST">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="body">Participate in the discussion: </label>
+                            <input type="text" name="body" rows="5" class="form-control" placeholder="Have something to say?" aria-describedby="helpId">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @else
+        <div class="row justify-content-center mt-4">
+            <p>Please <a href="{{ route('login') }}">sign in</a> to participate in the discussion</p>
+        </div>
+    @endif
 </div>
 @endsection
